41a6fd20baeef17736665594cac74a478a1033cf
Anzle Chavez <anzlemchavez@gmail.com>
2024-11-04 14:07:28 -0500

    Added comment for main loop

--- main.cpp
+++ main.cpp
@@ -1,5 +1,7 @@
 #include "ui.h"
 
+// TODO: Determine next actions based on the state of the robot in this function
+// 			 Consider a simple match statement.
 void runLoop(state_t &state) {
 	// STANDBY MODE: await RESTOCK or END_SHIFT command
 	ui_get_standby_state(state);
@@ -9,5 +11,6 @@ task main() {
 	state_t state;
 	init_state(state);
 	runLoop(state);
+
 	delay(5000); // TEMPORARY: 11/4
 }
