07c18d6d2ac43388a9c2897aa23f7d19910d734b
Anzle Chavez <anzlemchavez@gmail.com>
2024-12-01 17:50:31 -0500

    main.cpp documented

--- main.cpp
+++ main.cpp
@@ -4,38 +4,47 @@
 #include "ui.h"
 #include "types.h"
 
+// void runLoop(state_t &state)
+// state: The global state struct
+// After the robot is initialized inside task main(), runLoop acts as a jump table, and delegates
+// execution to the appropriate state function for the current state.
 
 void runLoop(state_t &state) {
 	// STANDBY MODE: await RESTOCK or END_SHIFT command
-	if (state.mode == 0) {
+	if (state.mode == Standby) {
 		ui_standby(state);
 	}
 
 	// RESTOCK MODE: robot indexes items
-	else if (state.mode == 1) {
+	else if (state.mode == Restock) {
 		ind_loading(state);
 	}
 
 	// END_SHIFT MODE: call shutdown procedure
-	else if (state.mode == 2) {
+	else if (state.mode == End_Shift) {
 		shutdown();
 	}
 
 	// ROUTE MODE: routing and line following
-	else if (state.mode == 3) {
+	else if (state.mode == Route) {
 		flr_route(state);
 	}
-
-	// DELIVER: moving box off robot
-	else if (state.mode == 4) {
-		//dvr_deliver(state);
-	}
 }
+ 
+// task killSwitch()
+// Our robot, given that it is the only robot during the demo, we decided that a physical e-stop 
+// would be beneficial by helping us immediately stop execution, and allowed us to test our robot 
+// faster. It relies on a touch sensor, easily accessible on the top of the robot, to stop
+// execution.
 
-task killswitch() {
+task killSwitch() {
 	while(!getTouchValue(S1)){}
 	stopAllTasks();
 }
+
+// task main()
+// Logic is kept to a minimum in here. Other than initializing the states, the main() function calls
+// runLoop(state_t &state), which delegates the execution to the appropriate state function.
 
 task main() {
 	init_program(); // Initializes system
